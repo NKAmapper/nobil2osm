f65c456ba72e7bbf94aa13f34a3c451974d631d6
NKA <nkamapper@gmail.com>
2021-01-26 18:41:08 +0100

    Updated for new brands, capacities

--- nobil2osm.py
+++ nobil2osm.py
@@ -14,7 +14,41 @@ import re
 import urllib2
 
 
-version = "0.9.5"
+version = "0.10.0"
+
+
+capacities = {
+	'7':  3.6, # 3,6 kW . 230V 1-phase max 16A
+	'16': 6.3, # 230V 3-phase max 16A
+	'8':  7.4, # 7.4 kW - 230V 1-phase max 32A
+	'10': 11,  # 11 kW - 400V 3-phase max 16A
+	'17': 13,  # 230V 3-phase max 32A
+	'19': 20,  # 20 KW - 500VDC max 50A
+	'11': 22,  # 22 kW - 400 3-phase max 32A
+	'18': 25,  # 230V 3-phase max 63A
+	'12': 43,  # 43 kW - 400 3-phase max 63A	
+	'13': 50,  # 50 kW - 500VDC max 100A
+	'20': 50,  # Less then 100 kW + 43 kW - 500VDC max 200A + 400V 3-phase max 63A
+	'21': 50,  # Less then 100 kW + 22 kW - 500VDC max 50A + 400V 3-phase max 32A
+	'28': 50,  # 50 kW - 400VDC max 125A
+	'29': 75,  # 75 kW DC
+	'23': 100, # 100 kW - 500VDC max 200A
+	'22': 135, # 135 kW - 480 VDC max 270A
+	'24': 150, # 150 kW DC
+	'30': 225, # 225 kW DC
+	'31': 250, # 250 kW DC
+	'25': 350, # 350 kW DC
+	'26': 350, # 350 bar
+	'0':  0}   # Unspecified
+
+
+
+# Output message to console
+
+def message (text):
+
+	sys.stderr.write(text)
+	sys.stderr.flush()
 
 
 # Produce a tag for OSM file
@@ -31,28 +65,11 @@ def make_osm_line(key,value):
 
 def find_capacity(capacity_id):
 
-	capacity = {
-		'7':  3.6, # 3,6 kW . 230V 1-phase max 16A
-		'16': 6.3, # 230V 3-phase max 16A
-		'8':  7.4, # 7.4 kW - 230V 1-phase max 32A
-		'10': 11,  # 11 kW - 400V 3-phase max 16A
-		'17': 13,  # 230V 3-phase max 32A
-		'19': 20,  # 20 KW - 500VDC max 50A
-		'11': 22,  # 22 kW - 400 3-phase max 32A
-		'12': 43,  # 43 kW - 400 3-phase max 63A	
-		'13': 50,  # 50 kW - 500VDC max 100A
-		'23': 100, # 100 kW - 500VDC max 200A
-		'22': 135, # 135 kW - 480 VDC max 270A
-		'24': 150, # 150 kW DC
-		'25': 350, # 350 kW DC
-		'20': 50,  # Less then 100 kW + 43 kW - 500VDC max 200A + 400V 3-phase max 63A
-		'21': 50,  # Less then 100 kW + 22 kW - 500VDC max 50A + 400V 3-phase max 32A
-		'0':  0}   # Unspecified
-
-	try:
+	if capacity_id in capacity:
 		return capacity[capacity_id]
-	except KeyError:
-		raise KeyError('Unknown capacity_id: "%s"' % capacity_id)
+	else:
+		message('Unknown capacity_id: "%s"\n' % capacity_id)
+		return 0
 
 
 # Main program
@@ -75,7 +92,7 @@ if __name__ == '__main__':
 	node_id = -1000
 	position = ()
 
-	# Loop through all charging stations and produce OSM tags
+	# Loop all charging stations and produce OSM tags
 
 	for station in nobil_data['chargerstations']:
 
@@ -85,10 +102,9 @@ if __name__ == '__main__':
 
 		print('  <node id="' + str(node_id) + '" lat="' + str(position[0]) + '" lon="' + str(position[1]) + '">')
 
-		make_osm_line("amenity","charging_station")
+#		make_osm_line("amenity","charging_station")  # Moved to end due to hydrogen
 		make_osm_line("ref:nobil",str(station['csmd']['id']))
-		make_osm_line("operator",station['csmd']['Owned_by'])
-		make_osm_line("source","nobil.no")
+#		make_osm_line("source","nobil.no")
 
 		make_osm_line("ADDRESS",station['csmd']['Street'] + " " + station['csmd']['House_number'] + ", " +\
 								station['csmd']['Zipcode'] + " " + station['csmd']['City'])
@@ -99,7 +115,7 @@ if __name__ == '__main__':
 		make_osm_line("COUNTY",station['csmd']['County'])
 		make_osm_line("COUNTRY",station['csmd']['Land_code'])
 
-#		make_osm_line("CREATED",station['csmd']['Created'][:10])
+		make_osm_line("CREATED",station['csmd']['Created'][:10])
 		make_osm_line("UPDATED",station['csmd']['Updated'][:10])
 
 #		make_osm_line("CHARGING_POINTS",str(station['csmd']['Number_charging_points']))
@@ -117,8 +133,9 @@ if __name__ == '__main__':
 			description = station['csmd']['User_comment']
 
 		description = " ".join(description.split())
-		make_osm_line("description",description)
+		make_osm_line("DESCRIPTION",description)
 
+		'''
 		# Generate contact email tag
 
 #		make_osm_line("CONTACT",station['csmd']['Contact_info'])
@@ -180,6 +197,7 @@ if __name__ == '__main__':
 				website = "http://" + website
 			if website != "http://e.on":	
 				make_osm_line("contact:website", website)
+		'''
 
 		# Loop each extra information item
 
@@ -204,7 +222,7 @@ if __name__ == '__main__':
 					make_osm_line("access", "residents")
 
 				else:
-					raise KeyError('Unknown availability id: "%s"' % info['attrvalid'])
+					message('Unknown availability id: "%s" - %s\n' % (info['attrvalid'], info['trans']))
 
 			# Produce OSM tag for location
 
@@ -244,7 +262,7 @@ if __name__ == '__main__':
 #					make_osm_line("TIME_LIMIT", "no")
 
 
-		# Loop thorough all connectors for station and fetch info
+		# Loop all connectors for station and fetch info
 
 		sockets = {
 			'schuko': 0,
@@ -296,55 +314,74 @@ if __name__ == '__main__':
 			'short': False
 			}
 
+		hydrogen = False
+
 		capacity_adjustment = 0
 
 		for connector in station['attr']['conn'].values():
 
-			if '4' in connector:
+			if '4' in connector:  # "Connector"
+
+				# Find capacity
+
+				if "5" in connector:
+					if connector['5']['attrvalid'] in capacities:
+						connector_capacity = capacities[ connector['5']['attrvalid'] ]
+
+					elif connector['5']['attrvalid'] in ['26', '27']:  # Hydrogen
+						hydrogen = True
+						connector_capacity = 0
+					else:
+						connector_capacity = 0
+						message('Unexpected capacity: "%s" - %s\n' % (connector['5']['attrvalid'], connector['5']['trans']))
+						make_osm_line("FIXME", "Socket output (kW)")
+				else:
+					connector_capacity = 0
+
 
 				# Increase number of connectors by 1 and set capacity
 
 				if connector['4']['attrvalid'] == '14':  # Schuko / Schucko CEE 7/4
 					sockets['schuko'] += 1
 					if '5' in connector:
-						capacity['schuko'] = max(capacity['schuko'],find_capacity(connector['5']['attrvalid']))
+						capacity['schuko'] = max(capacity['schuko'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '29':  # Tesla Connector Roadster
 					sockets['tesla_roadster'] += 1
 					if '5' in connector:
-						capacity['tesla_roadster'] = max(capacity['tesla_roadster'],find_capacity(connector['5']['attrvalid']))
+						capacity['tesla_roadster'] = max(capacity['tesla_roadster'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '34':  # Blue industrial 3-pin
 					sockets['cee_blue'] += 1
 					if '5' in connector:
-						capacity['cee_blue'] = max(capacity['cee_blue'],find_capacity(connector['5']['attrvalid']))
+						capacity['cee_blue'] = max(capacity['cee_blue'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '35':  # Blue industrial 4-pin
 					sockets['cee_blue'] += 1
 					if '5' in connector:
-						capacity['cee_blue'] = max(capacity['cee_blue'],find_capacity(connector['5']['attrvalid']))
+						capacity['cee_blue'] = max(capacity['cee_blue'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '36':  # Red industrial 5-pin
 					sockets['cee_red'] += 1
 					if '5' in connector:
-						capacity['cee_red'] = max(capacity['cee_red'],find_capacity(connector['5']['attrvalid']))
+						capacity['cee_red'] = max(capacity['cee_red'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '31':  # Type 1
 					sockets['type1'] += 1
 					if '5' in connector:
-						capacity['type1'] = max(capacity['type1'],find_capacity(connector['5']['attrvalid']))
+						capacity['type1'] = max(capacity['type1'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '32':  # Type 2
 					sockets['type2'] += 1
 					if '5' in connector:
-						capacity['type2'] = max(capacity['type2'],find_capacity(connector['5']['attrvalid']))
+						capacity['type2'] = max(capacity['type2'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '60':  # Type 1/Type 2
 					sockets['type1'] += 1
 					sockets['type2'] += 1
 					capacity_adjustment +=1
 					if '5' in connector:
-						capacity['type2'] = max(capacity['type2'],find_capacity(connector['5']['attrvalid']))
+						capacity['type2'] = max(capacity['type2'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '50':  # Type 2 + Chucko CEE 7/4
 					sockets['schuko'] += 1
@@ -352,36 +389,36 @@ if __name__ == '__main__':
 					capacity_adjustment +=1
 					capacity['schuko'] = max(capacity['schuko'],3.6)
 					if '5' in connector:
-						capacity['type2'] = max(capacity['type2'],find_capacity(connector['5']['attrvalid']))
+						capacity['type2'] = max(capacity['type2'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '39':  # CCS/Combo
 					sockets['type2_combo'] += 1
 					if '5' in connector:
-						capacity['type2_combo'] = max(capacity['type2_combo'],find_capacity(connector['5']['attrvalid']))
+						capacity['type2_combo'] = max(capacity['type2_combo'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '41':  # Combo + CHAdeMO
 					sockets['type2_combo'] += 1
 					sockets['chademo'] += 1
 					if '5' in connector:
-						capacity['type2_combo'] = max(capacity['type2_combo'],find_capacity(connector['5']['attrvalid']))
-						capacity['chademo'] = max(capacity['chademo'],find_capacity(connector['5']['attrvalid']))
+						capacity['type2_combo'] = max(capacity['type2_combo'],connector_capacity)
+						capacity['chademo'] = max(capacity['chademo'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '42':  # CHAdeMO + Type 2
 					sockets['chademo'] += 1
 					sockets['type2'] += 1
 					capacity_adjustment += 1
 					if '5' in connector:
-						capacity['chademo'] = max(capacity['chademo'],find_capacity(connector['5']['attrvalid']))
+						capacity['chademo'] = max(capacity['chademo'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '30':  # CHAdeMO
 					sockets['chademo'] += 1
 					if '5' in connector:
-						capacity['chademo'] = max(capacity['chademo'],find_capacity(connector['5']['attrvalid']))
+						capacity['chademo'] = max(capacity['chademo'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '40':  # Tesla Connector
 					sockets['tesla_supercharger'] += 1
 					if '5' in connector:
-						capacity['tesla_supercharger'] = max(capacity['tesla_supercharger'],find_capacity(connector['5']['attrvalid']))
+						capacity['tesla_supercharger'] = max(capacity['tesla_supercharger'],connector_capacity)
 
 				elif connector['4']['attrvalid'] == '43':  # CHAdeMO + Combo + AC-Type2
 					sockets['chademo'] += 1
@@ -403,11 +440,14 @@ if __name__ == '__main__':
 							capacity['type2'] = max(capacity['chademo'],43)
 
 						else:
-							capacity['chademo'] = max(capacity['chademo'],find_capacity(connector['5']['attrvalid']))					
-							capacity['type2_combo'] = max(capacity['type2_combo'],find_capacity(connector['5']['attrvalid']))					
+							capacity['chademo'] = max(capacity['chademo'],connector_capacity)					
+							capacity['type2_combo'] = max(capacity['type2_combo'],connector_capacity)
+
+				elif connector['4']['attrvalid'] == '70':  # Hydrogen
+					hydrogen = True	
 
 				elif connector['4']['attrvalid'] != "0":   # Unspecified
-					raise KeyError('Unexpected connector: "%s"' % connector['4']['attrvalid'])		
+					message('Unexpected connector: "%s" - %s\n' % (connector['4']['attrvalid'], connector['4']['trans']))
 
 				# Fetch accessibility / authentication info for connector
 
@@ -429,7 +469,7 @@ if __name__ == '__main__':
 						authentication['cellular'] = True
 
 					elif connector['1']['attrvalid'] != '3':  # Not Other
-						raise KeyError('Unexpected Accessibility: "%s"' % connector['1']['attrvalid'])
+						message('Unexpected accessibility: "%s" - %s\n' % (connector['1']['attrvalid'], connector['1']['trans']))
 
 				# Fetch payment info for connector
 
@@ -467,7 +507,7 @@ if __name__ == '__main__':
 						payment['cards'] = True				
 
 					elif connector['19']['attrvalid'] != '10': # Not Miscellaneous
-						raise KeyError('Unexpected Payment method: "%s"' % connector['19']['attrvalid'])
+						message('Unexpected payment method: "%s" - %s\n' % (connector['19']['attrvalid'], connector['19']['trans']))
 
 				# Fetch vehicle type
 
@@ -482,8 +522,15 @@ if __name__ == '__main__':
 						vehicle['plugin'] = True
 					elif connector['17']['attrvalid'] == '6':  # Van
 						vehicle['van'] = True
+					elif connector['17']['attrvalid'] in ['7','8','9']:
+						hydrogen = True
 					else:
-						raise KeyError('Unexpected Vehicle type: "%s"' % connector['17']['attrvalid'])
+						message('Unexpected vehicle type: "%s" - %s\n' % (connector['17']['attrvalid'], connector['17']['trans']))
+
+			# Check for hydrogen
+
+			if '26' in connector and connector['26']['attrvalid'] == "2":
+				hydrogen == True
 
 
 		# Produce osm tags with number of sockets per connector type
@@ -503,8 +550,11 @@ if __name__ == '__main__':
 		# Estimate capacity and produce osm tag
 
 		est_capacity = max(sockets['chademo'], sockets['type2_combo']) + sum(sockets.itervalues()) - sockets['chademo'] - sockets['type2_combo'] - capacity_adjustment
-		make_osm_line("capacity", str(est_capacity))
 
+		if est_capacity > 0:
+			make_osm_line("capacity", str(est_capacity))
+
+		'''
 		# Produce osm tags for authentication
 
 		if authentication['open']:
@@ -517,7 +567,7 @@ if __name__ == '__main__':
 			make_osm_line("authentication:nfc", "yes")
 
 		if authentication['cellular']:
-			make_osm_line("authentication:mobile_phone", "yes")
+			make_osm_line("authentication:app", "yes")
 
 		if authentication['payment']:
 			make_osm_line("authentication:payment", "yes")
@@ -534,7 +584,7 @@ if __name__ == '__main__':
 			make_osm_line("payment:contactless", "yes")
 
 		if payment['cellular']:
-			make_osm_line("payment:mobile_phone", "yes")
+			make_osm_line("payment:app", "yes")
 
 		if payment['subscription']:
 			make_osm_line("payment:subscription", "yes")
@@ -547,6 +597,7 @@ if __name__ == '__main__':
 
 		if True in payment.values() or authentication['payment']:
 			make_osm_line("fee", "yes")
+		'''
 
 		# Produce osm access tags for some vehicle types
 
@@ -578,21 +629,46 @@ if __name__ == '__main__':
 
 		# Insert network name at the start of station name
 
+		'''
 		network_list = [
 			('Fortum','chargedrive'),
 			(u'Grønn kontakt','gronnkontakt'),
-			('Clever',''),
 			('Tesla',''),
 			('BKK','bilkraft'),
 			('Charge365',''),
 			(u'Laddregion Mälardalen',''),
 			('InCharge',''),
-			('Ionity','') ]
+			('Ionity',''),
+			(u'Göteborg Energi', ''),
+			('Bee', 'clever'),
+			('Circle K', ''),
+			('E\.ON', 'eon'), ]  # . is special character
+		'''
+
+		network_list = [
+			('Fortum','Fortum'),
+			('Recharge', 'Fortum'),
+			(u'Grønn kontakt', u'Grønn kontakt'),
+			('Tesla', 'Tesla'),
+			('BKK', 'BKK'),
+			('BKK/Lyse', 'BKK'),
+			('Charge365', 'Charge365'),
+			('InCharge', 'InCharge'),
+			('Ionity', 'Ionity'),
+			(u'Göteborg Energi', u'Göteborg Energi'),
+			('Bee', 'Bee'),
+			('Circle K', 'Circle K'),
+			('E.ON', 'E.ON'), ]
 
 		network_name = ""
 
 		for network in network_list:
+			if station['csmd']['Owned_by'] == network[0]:
+				name = network[1] + " " + name.replace(network[0], "").lstrip(", ").strip()
+				network_name = network[1]
+				break
 
+			'''
 			# First remove any existing network name from station name
 			reg1 = re.search(r'\b(%s)\b' % network[0], name, flags=re.IGNORECASE|re.UNICODE)
 			if reg1:
@@ -612,8 +688,10 @@ if __name__ == '__main__':
 				reg6 = None
 
 			if reg1 or reg2 or reg3 or reg4 or reg5 or reg6:
-				name = network[0] + " " + name.lstrip(", ")
-				network_name = network[0]
+				name = network[0].replace("\\", "") + " " + name.lstrip(", ")
+				network_name = network[0].replace("\\", "")
+				break
+			'''
 
 		# Lowercase certain words; uppercase others (or a mix)
 
@@ -642,16 +720,16 @@ if __name__ == '__main__':
 		name = name.replace("  "," ")
 		name = name.strip(" ")
 
-		if name[0:6] != "eRoute":
+		if name[0:6] != "eRoute" and len(name) > 1:
 			name = name[0].upper() + name[1:]
 
 		# Remove municipality name at the end (for Norway only)
 
 		if station['csmd']['Land_code'] == "NOR":
-			split_name = name.split(" ")
-			network_length = len(network_name.split(" "))
-			if len(split_name) > network_length and split_name[len(split_name)-1].upper() == station['csmd']['Municipality']:
-				name = name.rstrip(split_name[len(split_name)-1])
+			split_name = name.split()
+			network_length = len(network_name.split())
+			if len(split_name) > network_length and split_name[-1].upper() == station['csmd']['Municipality'] and ", " in name:
+				name = name.rstrip(split_name[-1])
 				name = name.strip(", ")
 
 		# Produce osm tags for name and network
@@ -660,9 +738,21 @@ if __name__ == '__main__':
 
 		if network_name != "":
 			make_osm_line("brand",network_name)
+		else:
+			make_osm_line("operator",station['csmd']['Owned_by'])
 
 		if name != original_name:
 			make_osm_line("NOBIL_NAME",original_name)
+
+		# Produce amnity station tag
+
+		if not hydrogen or sockets:
+			make_osm_line("amenity", "charing_station")
+		else:
+			make_osm_line("amenity", "fuel_station")
+
+		if hydrogen:
+			make_osm_line("fuel:hydrogen", "yes")
 
 		# Done with OSM station node
 
